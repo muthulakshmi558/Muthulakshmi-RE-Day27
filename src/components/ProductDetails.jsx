5f6df8d0b3a5f13cae33b9b27a90a05da2fbc964
Muthulakshmi <muthulakshmi5293@gmail.com>
2025-06-20 12:37:34 +0530

    Update ProductDetails.jsx

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -4,7 +4,7 @@ const ProductDetails = () => {
   return (
     <div>
       <h2>Apple iPhone 12</h2>
-      <img src="../assets/iphone.jpg" alt="Product"  className="product-img"/>
+      <img src="./assets/iphone.jpg" alt="Product"  className="product-img"/>
       <p>Price: ₹79,999</p>
       <p>Description: Latest iPhone with A12 Bionic chip and OLED display.</p>
     </div>
